cf9eae420b99b81b84343134431d4de4f8b581dc
Mathew Davidov <41017026+MathewDavidov@users.noreply.github.com>
2021-04-21 16:53:19 -0400

    Add audio lab

---
+++ labs/Audio/audio.js
@@ -0,0 +1,24 @@
+const rain = document.getElementById("rain");
+const playRain = () => {
+  rain.play();
+};
+
+const explosion = document.getElementById("explosion");
+const playExplosion = () => {
+  explosion.play();
+};
+
+const thunder = document.getElementById("thunder");
+const playThunder = () => {
+  thunder.play();
+};
+
+const piano = document.getElementById("piano");
+const playPiano = () => {
+  piano.play();
+};
+
+const guitar = document.getElementById("guitar");
+const playGuitar = () => {
+  guitar.play();
+};
